4a2c6ac06951b1765c4e686e8d971886a281a56c
TE23D-Eriksson-David <david.eriksson2@elev.ga.ntig.se>
2025-03-14 10:53:50 +0100

    implimenterat Java script då Den stora produkt beskrivningen nu fungerar.

--- script.js
+++ script.js
@@ -1,55 +1,14 @@
-function DMSBFDFM(a){
-    if (a=1) {
-        alert("'DMSBFDFM' Den Metod Som Bara Finns Där För Mig =)")
-    } else {
-        alert("b") // test
-    }
-}
-
-// Funkar ej ska fixas
-window.ChangeArow = function(vilkenPill){ // det här är idiotiskt att jag behövde strulla så mycket.
-// Efftersom anropet låg i massa element så behövde den vara global med window... Påminelse till senare 
-    switch (vilkenPill) {
-        case 0:
-            alert("rotate kinda work")
-        break;
-        case 1:
-            let arrowProperty = document.querySelector("#Arrowimg1")
-            arrowProperty.style.transform = "rotate(+180deg)"
-        break;
-        case 2:
-            let arrowProperty2 = document.querySelector("#Arrowimg2")
-            arrowProperty2.style.transform = "rotate(180deg)"
-        break;
-        case 3:
-            let arrowProperty3 = document.querySelector("#Arrowimg3")
-            arrowProperty3.style.transform = "rotate(180deg)"
-        break;
-        case 4:
-            let arrowProperty4 = document.querySelector("#Arrowimg4")
-            arrowProperty4.style.transform = "rotate(180deg)"
-        break;
-    }
-}
-
-
-
-
-
-
 function ändraSpråk(){ // Problem också
-
+alert("Den här fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
 }
 
 function AlertaIngenMeny(){
-
+alert("Den här menyn fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
 }
+
+
 
 function TillVarukorgen(){
-
-}
-
-function ChangeArow(){
 
 }
 
@@ -57,6 +16,42 @@ function LäggTillVarukorg(){
 
 }
 
-function MerInfoProduckt(){
 
+
+
+
+
+
+
+function MerInfoProduckt(ImgID){
+
+let strimg = "#imgProduckt"+ImgID
+        let taking_src_elementet = document.querySelector(strimg)
+        let taking_alt_elementet = document.querySelector(strimg)
+
+        const newScr = taking_src_elementet.getAttribute("src");
+        const newAlt = taking_alt_elementet.getAttribute("alt");
+
+        let Recivning_img_element = document.querySelector("#Stor_img")
+        Recivning_img_element.setAttribute("src", newScr);
+        Recivning_img_element.setAttribute("alt", newAlt);
+
+let strPris = "#productPris"+ImgID
+    let ProductPris = document.querySelector(strPris)
+    let Recivning_pris_element = document.querySelector("#Stor_productPris")
+    Recivning_pris_element.textContent = ProductPris.textContent
+
+let strPbes = "#productBeskrivning"+ImgID
+    let ProductBes = document.querySelector(strPbes)
+    let Recivning_Bes_element = document.querySelector("#Stor_productBeskrivning")
+    Recivning_Bes_element.textContent = ProductBes.textContent 
+}
+
+
+function DMSBFDFM(a){
+    if (a===1) {
+        alert("'DMSBFDFM' Den Metod Som Bara Finns Där För Mig =)")
+    } else {
+        alert("b") // test
+    }
 }
