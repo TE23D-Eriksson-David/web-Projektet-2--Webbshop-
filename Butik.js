35e3c5d5932cffa93c8911cc588301b4d0397569
TE23D-Eriksson-David <david.eriksson2@elev.ga.ntig.se>
2025-03-16 00:21:18 +0100

    Nu borde all JS kod vara skriven då jag har skappat alla funktioner och troligtviss fixat lite CSS också jag vet att deta inte är det bästa medellandet men jag kan leva med det efftersom klokan är 00:21

---
+++ Butik.js
@@ -0,0 +1,69 @@
+function ändraSpråk(){ // Problem också
+    alert("Den här fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
+}
+
+function AlertaIngenMeny(){
+    alert("Den här menyn fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
+}
+
+function TillVarukorgen(page) {
+    window.open(page, "_self");
+}
+
+function LäggTillVarukorg(ProduktId){
+
+    let strimg = "#imgProduckt"+ProduktId
+        let taking_src_elementet = document.querySelector(strimg)
+        let taking_alt_elementet = document.querySelector(strimg)
+
+    let strPris = "#productPris"+ProduktId
+        let ProductPris = document.querySelector(strPris)
+
+    let strPbes = "#productBeskrivning"+ProduktId
+        let ProductBes = document.querySelector(strPbes)
+        
+
+    let ProducktInfo={
+        Pris: ProductPris,
+        Beskrivning: ProductBes,
+        BildAlt: taking_alt_elementet,
+        BildSrc: taking_src_elementet
+    }
+
+    const Json_ProducktInfo = JSON.stringify(ProducktInfo)
+    window.localStorage.setItem(ProduktId,Json_ProducktInfo)
+}
+
+
+function MerInfoProduckt(ImgID){
+
+    let strimg = "#imgProduckt"+ImgID
+            let taking_src_elementet = document.querySelector(strimg)
+            let taking_alt_elementet = document.querySelector(strimg)
+
+            const newScr = taking_src_elementet.getAttribute("src");
+            const newAlt = taking_alt_elementet.getAttribute("alt");
+
+            let Recivning_img_element = document.querySelector("#Stor_img")
+            Recivning_img_element.setAttribute("src", newScr);
+            Recivning_img_element.setAttribute("alt", newAlt);
+
+    let strPris = "#productPris"+ImgID
+        let ProductPris = document.querySelector(strPris)
+        let Recivning_pris_element = document.querySelector("#Stor_productPris")
+        Recivning_pris_element.textContent = ProductPris.textContent
+
+    let strPbes = "#productBeskrivning"+ImgID
+        let ProductBes = document.querySelector(strPbes)
+        let Recivning_Bes_element = document.querySelector("#Stor_productBeskrivning")
+        Recivning_Bes_element.textContent = ProductBes.textContent 
+}
+
+
+function DMSBFDFM(a){
+    if (a===1) {
+        alert("'DMSBFDFM' Den Metod Som Bara Finns Där För Mig =)")
+    } else {
+        alert("b") // test
+    }
+}
---
+++ Varukorg.js
@@ -0,0 +1,78 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    NyProduckt()
+})
+
+
+function ändraSpråk(){ // Problem också
+    alert("Den här fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
+    }
+    
+function AlertaIngenMeny(){
+    alert("Den här menyn fungerar inte. Jag orkar inte och har inte tid med att implimentera det")
+    }
+
+function TillButik(page) {
+        window.open(page, "_self");
+    }
+
+
+
+function NyProduckt(){
+
+    for (let index = 1; index < 7; index++) {
+        const nyckel = index
+        let OParsadJson = window.localStorage.getItem(nyckel)
+        if (OParsadJson) {
+            const ProducktInfo = JSON.parse(OParsadJson)
+            CreateProduct(ProducktInfo)
+        }
+    }
+
+
+function CreateProduct(ProducktInfo){
+        
+        let NewProductArt = document.createElement("article")
+        let NewProductImg = document.createElement("img")
+        let NewDiv = document.createElement("div")
+        let NewPris = document.createElement("p")
+        let NewBeskrivning = document.createElement("p")
+        let NewButton = document.createElement("button")
+        let NewButtonImg = document.createElement("img")
+
+        
+        NewProductImg.setAttribute("src", ProducktInfo.BildSrc)
+        NewProductImg.setAttribute("alt", ProducktInfo.BildAlt)
+        NewPris.textContent = ProducktInfo.Pris
+        NewBeskrivning.textContent = ProducktInfo.Beskrivning
+        NewButtonImg.setAttribute("src", "./img/Kryss.png")
+        NewButtonImg.setAttribute("alt", "Bortagnigs kryss")
+
+        NewProductArt.classList.add("Allclass")
+        NewProductImg.classList.add("Classimg")
+        NewDiv.classList.add("Prducbes")
+        NewButton.classList.add("productVarukorgknapp")
+        NewButtonImg.classList.add("Varukorgimg")
+    
+        NewProductArt.appendChild(NewProductImg)
+        NewProductArt.appendChild(NewDiv)
+        NewDiv.appendChild(NewPris)
+        NewDiv.appendChild(NewBeskrivning)
+        NewButton.appendChild(NewButtonImg)
+        NewProductArt.appendChild(NewButton)
+
+    };
+
+}
+
+
+function Betalning(){
+    alert("Du har inte råd")
+    }
+
+function TömVarukorg(){
+    let article = document.querySelectorAll("article")
+    article.forEach(function(article){
+        article.parentNode.removeChild(article)
+    })
+}
